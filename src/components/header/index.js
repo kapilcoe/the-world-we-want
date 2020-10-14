d3f59e613af358fada0521525fbfcd5dbfe73b7a
Maitri Pathak <maitripathak@Maitris-MacBook-Air.local>
2020-10-14 19:59:40 +0530

    Header Logo and Hamburger Layout

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,20 +3,21 @@ import './header.css';
 
 export default ({}) => {
     return <div className="header-container">
-        <nav>
-            <div className="img-icon">
-                <img src=""></img>
-            </div>
-            <ul className="nav-links">
-                <li><a href="#">Facebook</a></li>
-                <li><a href="#">Insta</a></li>
-                <li><a href="#">Twitter</a></li>
-            </ul>
-            <div className="hamburger">
-                <div class="line"></div>
-                <div class="line"></div>
-            </div>
-        </nav>
+        <header>
+            <img className="logo"></img><p>The World We Want</p>
+            <nav>
+                <ul className="nav-area">
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Insta</a></li>
+                    <li><a href="#">Twitter</a></li>
+                </ul>
+              
+            </nav>
+            <a href="#" className="btn-area">Hamburger</a>
+
+
+        </header>
+       
 
         
     </div>
