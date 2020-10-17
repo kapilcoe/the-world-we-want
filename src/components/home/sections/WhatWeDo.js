5af70034af6e2ae112442900dab1f520398ec4e0
Maitri Pathak <maitripathak@Maitris-MacBook-Air.local>
2020-10-18 03:00:05 +0530

    WhatWEDo animation

--- src/components/home/sections/WhatWeDo.js
+++ src/components/home/sections/WhatWeDo.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import whatwedo1 from '../../../images/Whatwedo_1.png';
 import whatwedo2 from '../../../images/Whatwedo_2.png';
-
+import {imagetilt} from '../../../utils';
 
 export default ({}) => {
+    useEffect(() => {
+        imagetilt();
+    }, []);
     return <div class='whatwedo-section-container'>
         <div className="title">WHAT WE DO</div>
         <div class='description'>Through hero ventures,  accelerators and tapping our areas of expertise, we help make the global goals a part of everyday life and lingo</div>
