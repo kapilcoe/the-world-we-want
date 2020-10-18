a1a3a437e3ca534e045b72826dc513eab69e0b9f
kapilcoe <kapil.coe@gmail.com>
2020-10-19 00:46:42 +0530

    images compressed

--- src/components/home/HeroBanner.js
+++ src/components/home/HeroBanner.js
@@ -9,5 +9,5 @@ export default ({}) => {
         //bannerZoomIn();
     }, []);
     const props = useSpring({from: {opacity: 0}, opacity: 1});
-    return <animated.div className="banner-image"><img className='banner-image-img' src={HeroBanner} alt='the world we want'/></animated.div>
+    return <div className="banner-image"><img className='banner-image-img' src={HeroBanner} alt='the world we want'/></div>
 }
