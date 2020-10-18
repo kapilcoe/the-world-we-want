6690ff7edad095e3272be5a742a34c600b3f85ba
kapilcoe <kapil.coe@gmail.com>
2020-10-19 00:33:27 +0530

    news section css fix

--- src/components/home/sections/GoodNews.js
+++ src/components/home/sections/GoodNews.js
@@ -9,7 +9,7 @@ export default ({}) => {
         <div className="news-header show-on-scroll">GOOD NEWS</div>
             <div className="news-container">
                 <div className="news-area">
-                    <img height="250px" className="news-img" src={news_1}></img>
+                    <img height="400px" width='518px' className="news-img" src={news_1}></img>
                     <div className="news-content">
                         <div className="news-title">#WeAreFamily </div>
                         <div className="news-description">The World We Want, in conjunction with Kim Sledge, are releasing a Special Edition Cover version of the group’s timeless hit “We Are Family”, in benefit of the WHO Foundation, and with the support of the World Health Organization, to </div>
@@ -17,7 +17,7 @@ export default ({}) => {
                     
                 </div>
                 <div className="news-area">
-                    <img height="250px" className="news-img"  src={news_2}></img>
+                    <img height="400px" width='518px' className="news-img"  src={news_2}></img>
                     <div className="news-content">
                         <div className="news-title">#YOUNGA2020</div>
                         <div className="news-description">The World We Want are the Global Communications & PR partners of YOUNGA2020. Organized by BridgingTheGap Ventures, this first-of-its-kind virtual global youth takeover of the United Nations is focused on a central theme for 2020—the future youth want, the action we need.</div>
